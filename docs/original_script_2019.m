a1dcc5e1c239ea28edf5cbde84ef3c284ea32085
Nick Nuzzo <nnuzzo757@gmail.com>
2026-06-06 22:29:42 -0700

    Create original_script_2019.m

---
+++ docs/original_script_2019.m
@@ -0,0 +1,16 @@
+%% Solar intensity as a function of orbital position
+%
+% Original 2019 MATLAB/STK research script preserved in the conversation upload.
+% The refactored source version is in src/solar_flux_analysis.m.
+%
+% Original purpose:
+% Calculate solar flux incident on a satellite surface based on its position in orbit
+% and attitude by converting STK ECEF position data to latitude/longitude, estimating
+% ground track, and computing solar incidence on a nadir-pointing satellite face.
+%
+% Original sample inputs:
+% Start day number: 170.67
+% End day number:   170.78
+% Time step:        0.00073 days
+% Face:             0
+% Tilt angle:       0 deg
